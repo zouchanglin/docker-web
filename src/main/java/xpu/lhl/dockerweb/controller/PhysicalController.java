47df08fc8a9c10b8fa8f95f910d8f8d3cf6a5c9c
zouchanglin <zchanglin@aliyun.com>
2020-05-06 09:14:53 +0800

    feat:管理系统Docker化

--- src/main/java/xpu/lhl/dockerweb/controller/PhysicalController.java
+++ src/main/java/xpu/lhl/dockerweb/controller/PhysicalController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 @RequestMapping("/physical")
 public class PhysicalController {
